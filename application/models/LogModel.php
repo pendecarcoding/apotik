3be88b741498978d30277ae9290e60acbbe7dd3f
pendekarcoding <bohatimulyadi99@gmail.com>
2024-11-13 03:38:09 +0700

    add LogModel

---
+++ application/models/LogModel.php
@@ -0,0 +1,21 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+// application/models/LogModel.php
+class LogModel extends CI_Model {
+    public function addLog($userId, $action,$data=null) {
+        $data = [
+            'user_id' => $userId,
+            'action' => $action,
+            'data' => $data,
+            'timestamp' => date('Y-m-d H:i:s')
+        ];
+        $this->db->insert('user_logs', $data);
+    }
+
+    public function getAllLogs() {
+        $this->db->select('user_logs.*, users.first_name as name'); // Select fields, including user name
+        $this->db->from('user_logs');
+        $this->db->join('users', 'users.user_id = user_logs.user_id'); // Join with the users table on user ID
+        $this->db->order_by('user_logs.timestamp', 'DESC');
+        return $this->db->get()->result_array();
+    }
+}
